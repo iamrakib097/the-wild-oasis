603aa4c4f5f5c6fab000f53d418fbca509485cdb
Rakibul Islam <81356942+iamrakib097@users.noreply.github.com>
2024-09-08 01:02:11 +0600

    cabin section added and deletion

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://nvmvpyirhciueuvfccgp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52bXZweWlyaGNpdWV1dmZjY2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU3MjE0MzUsImV4cCI6MjA0MTI5NzQzNX0.4Y5Yze6trH4ADxX3vKKKXCA27gjbxqexuvuUdBeZGyY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

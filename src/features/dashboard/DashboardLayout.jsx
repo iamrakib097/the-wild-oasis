cea94306703e4b6900e9676549a9f82423fa0a2c
Rakibul Islam <81356942+iamrakib097@users.noreply.github.com>
2024-09-29 00:25:08 +0600

    Displaying statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import { useRecentBooking } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
+import { useCabins } from "../cabins/useCabins";
 // import { useRecentStays } from "./useRecentStays";
 import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -12,12 +14,23 @@ const StyledDashboardLayout = styled.div`
 
 const DashboardLayout = () => {
   const { bookings, isLoading: isLoading1 } = useRecentBooking();
-  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
-  if (isLoading1 || isLoading2) return <Spinner />;
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
   console.log(confirmedStays);
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Today&apos;s Activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>

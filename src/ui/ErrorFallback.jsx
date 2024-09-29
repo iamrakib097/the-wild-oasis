85217771f4b433b6156fd953c899ac6a26a73501
Rakibul Islam <81356942+iamrakib097@users.noreply.github.com>
2024-09-29 17:58:31 +0600

    [Add] Error Boundary

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -29,3 +29,27 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+import React from "react";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
+
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something wen wrong 😑</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+};
+
+export default ErrorFallback;
